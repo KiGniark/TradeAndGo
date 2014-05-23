5c9fbe069668b3dacf437d240cc9ce4d9170016c
KiGniark <euhhki@gmail.com>
2014-05-23 18:48:47 +0200

    Default activity splash

--- app/src/main/java/josiane/tradeandgo/app/Splash.java
+++ app/src/main/java/josiane/tradeandgo/app/Splash.java
@@ -11,7 +11,7 @@ public class Splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_synthese);
+        setContentView(R.layout.activity_splash);
     }
 
 
--- app/src/main/java/josiane/tradeandgo/app/info/Information.java
+++ app/src/main/java/josiane/tradeandgo/app/info/Information.java
@@ -16,7 +16,7 @@ public class Information extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_synthese);
+        setContentView(R.layout.activity_information);
     }
 
 
@@ -24,7 +24,6 @@ public class Information extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.synthese, menu);
         return true;
     }
 
