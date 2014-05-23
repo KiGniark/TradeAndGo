1665cb966d383dfd3a237189965e8fb08355aa13
KiGniark <euhhki@gmail.com>
2014-05-23 22:01:53 +0200

    splash et ui

--- app/src/main/java/josiane/tradeandgo/app/detail/Detail.java
+++ app/src/main/java/josiane/tradeandgo/app/detail/Detail.java
@@ -16,7 +16,7 @@ public class Detail extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_synthese);
+        setContentView(R.layout.activity_detail);
     }
 
 
